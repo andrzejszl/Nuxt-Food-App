abdb2d64f9224508b039d3e62019c1f8dc16becc
andrzejszl <andrzej.sz123@gmail.com>
2022-12-05 07:18:10 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="container">
+    <h1>Hi from home page!</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+html {
+  font-family: "Mulish", sans-serif;
+  font-weight: 300;
+  font-size: 16px;
+  word-spacing: 1px;
+  box-sizing: border-box;
+}
+*, *::before, *::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+h1, h2, h3, h4 {
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+}
+</style>
