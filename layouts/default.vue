4651c0134f789443efce0b6062511edda391f661
andrzejszl <andrzej.sz123@gmail.com>
2022-12-06 04:49:53 +0100

    create index page

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="content">
+            <AppMenu />
+            <Nuxt />
+        </div>
+    </div>
+</template>
+<script>
+import AppMenu from '@/components/AppMenu.vue';
+
+export default {
+    components: {
+        AppMenu
+    },
+};
+</script>
+
+<style>
+html {
+  font-family: "Mulish", sans-serif;
+  font-weight: 300;
+  font-size: 16px;
+  word-spacing: 1px;
+  box-sizing: border-box;
+}
+*, *::before, *::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+h1, h2, h3, h4 {
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,18 @@
 <template>
   <main class="container">
-    <h1>Hi from home page!</h1>
+    <AppHeader />
   </main>
 </template>
 
 <script>
+import AppHeader from '@/components/AppHeader.vue';
 export default {
-  name: 'IndexPage'
+  components: {
+    AppHeader
+  }
 }
 </script>
 
 <style>
-html {
-  font-family: "Mulish", sans-serif;
-  font-weight: 300;
-  font-size: 16px;
-  word-spacing: 1px;
-  box-sizing: border-box;
-}
-*, *::before, *::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-h1, h2, h3, h4 {
-  font-family: "Poppins", sans-serif;
-  font-weight: 600;
-}
+
 </style>
